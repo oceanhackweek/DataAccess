cdea01d17aa06f3c0ababcd57d71a7f63d97ac13
liutongya <liutongya@sio.org.cn>
2019-08-29 21:41:22 +0000

    update

--- isopy/isopy/isopy.py
+++ isopy/isopy/isopy.py
@@ -1,3 +1,4 @@
+# %load ../isopy/isopy.py
 import numpy as np
 import xarray as xr
 from scipy import interpolate
@@ -31,42 +32,42 @@ def compute_iso(data, iso, lev, d_or_t):
     N = size[0] #this should be the same length as lev
     
     #check that the input data is in the for depth lat lon (or depth lon lat)
-    if len(lev) ~= N:
+    if len(lev) != N:
         print('Error: either data input variable does not have depth as dimension 0, or lev variable is not same length as depth')
-        break
-    if d_or_t ~= 'd' or 't':
-        print('Is this density or temperature?')
-        break
-    var_iso = np.zeros((M, L)) # define the output var
-    var_iso[:, :] = np.nan # NaN fill to avoid any later computation errors with zeros
+    else:
+        if d_or_t == 'd' or 't':
+            var_iso = np.zeros((M, L)) # define the output var
+            var_iso[:, :] = np.nan # NaN fill to avoid any later computation errors with zeros
 
-    for i in np.arange(L): #loop through dimension 1
-        for j in np.arange(M): #loop through dimension 2
+            for i in np.arange(L): #loop through dimension 1
+                for j in np.arange(M): #loop through dimension 2
 
-                data_prof = data[:, j, i] #select one profile
-                if d_or_t == 'd':
-                    id1 = np.where(data_prof < iso)
-                    if np.size(id1) > 0 and np.size(id1) < len(z):
+                        data_prof = data[:, j, i] #select one profile
+                        if d_or_t == 'd':
+                            id1 = np.where(data_prof < iso)
+                            if np.size(id1) > 0 and np.size(id1) < len(z):
 
-                        var1 = data_prof[id1[0][-1]]
-                        var2 = data_prof[id1[0][-1] + 1]
+                                var1 = data_prof[id1[0][-1]]
+                                var2 = data_prof[id1[0][-1] + 1]
 
-                    if var1 < var2:
-                        func = interpolate.interp1d([var1, var2], [lev[id1[0][-1]], lev[id1[0][-1] + 1]])
-                        var_iso[j, i] = func(iso)
+                                if var1 < var2:
+                                    func = interpolate.interp1d([var1, var2], [lev[id1[0][-1]], lev[id1[0][-1] + 1]])
+                                    var_iso[j, i] = func(iso)
 
-                else：
-                    id1 = np.where(data_prof > iso)
-                        
-                    if np.size(id1) > 0 and np.size(id1) < len(z):
+                        else:
+                            id1 = np.where(data_prof > iso)
 
-                        var1 = data_prof[id1[0][-1]]
-                        var2 = data_prof[id1[0][-1] + 1]
+                            if np.size(id1) > 0 and np.size(id1) < len(z):
 
-                    if var1 > var2:
-                        func = interpolate.interp1d([var1, var2], [lev[id1[0][-1]], lev[id1[0][-1] + 1]])
-                        var_iso[j, i] = func(iso)
+                                var1 = data_prof[id1[0][-1]]
+                                var2 = data_prof[id1[0][-1] + 1]
 
+                                if var1 > var2:
+                                    func = interpolate.interp1d([var1, var2], [lev[id1[0][-1]], lev[id1[0][-1] + 1]])
+                                    var_iso[j, i] = func(iso)
+
+        else:
+            print('Is this density or temperature?')
     return var_iso
 
 
